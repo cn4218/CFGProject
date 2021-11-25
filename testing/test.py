894f07b7c779447f09b9fa582d009dfd7013ae67
Claire Boitet <claire.boitet@gmail.com>
2021-11-25 07:15:27 +0000

    added tests initialisation

--- testing/test.py
+++ testing/test.py
@@ -3,3 +3,18 @@ This is just a place holder,
 I think there will be different tests files 
 for different parts of the application 
 """
+
+# ############# Unit tests ##############
+import unittest
+
+class MyTestCase(unittest.TestCase):
+	def test_something(self):
+		self.assertEqual(True,False)  # add assertion here
+
+
+# ########## Regression tests ###########
+import test
+
+
+if __name__ == '__main__':
+	unittest.main()
