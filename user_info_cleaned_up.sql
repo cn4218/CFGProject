da3acf4e4a1775fd4d74965245d42dda0448c2ec
Nasian <87439662+nasiana@users.noreply.github.com>
2021-12-01 13:56:35 +0000

    Add files via upload

--- user_info_cleaned_up.sql
+++ user_info_cleaned_up.sql
@@ -6,4 +6,6 @@ CREATE TABLE `User_Info` (
 `Name` varchar(50) NOT NULL,
 `Email_Address` varchar(100) NOT NULL,
 CONSTRAINT PK_UserID PRIMARY KEY (User_ID)
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select * from user_info; 
