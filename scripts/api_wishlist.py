ab1424802c83977929f6cb1e1d21a85227522215
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-05 15:21:05 +0000

    Add files via upload

--- scripts/api_wishlist.py
+++ scripts/api_wishlist.py
@@ -14,28 +14,31 @@ def get_wishlist(user_id):
 def get_wishlist_item(user_id, product_id):
     wishlist_item = _get_wish_list_individual(user_id, product_id)
     return jsonify(wishlist_item)
-    
+
+
+
 @app.route('/wishlist',methods = ['PUT'])
 def add_wish_list():
-    wishlist = request.get_json()
+    wishlist_dict = request.get_json()
     add_wish_list(
-        UserID = wishlist['UserID'], 
-        ProductID = wishlist['ProductID'], 
-        Code_Wish = wishlist['Code_Wish'], 
-        Product_name = wishlist['Product_name'], 
-        Quantity = wishlist['Quantity'],
-        Brands = wishlist['Brands'], 
-        Brands_tags = wishlist['Brands_tags'], 
-        Categories_Tags = wishlist['Categories_Tags'], 
-        Countries_en = wishlist['Countries_en'], 
-        Ingredients_Text = wishlist['Ingredients_Text'], 
-        Image_url = wishlist['Image_url'], 
-        Image_Small_url = wishlist['Image_Small_url'], 
-        Image_Ingredients_url = wishlist['Image_Ingredients_url'],
-        Image_Ingredients_Small_url = wishlist['Image_Ingredients_Small_url'], 
-        Image_Nutrition_url = wishlist['Image_Nutrition_url'], 
-        Image_Nutrition_Small_url = wishlist['Image_Nutrition_Small_url']
+        Username = wishlist_dict['username'],
+        UserID = wishlist_dict['UserID'], 
+        ProductID = wishlist_dict['wishlist']['ProductID'], 
+        Code_Wish = wishlist_dict['wishlist']['Code_Wish'], 
+        Product_name = wishlist_dict['wishlist']['Product_name'], 
+        Quantity = wishlist_dict['wishlist']['Quantity'],
+        Brands = wishlist_dict['wishlist']['Brands'], 
+        Brands_tags = wishlist_dict['wishlist']['Brands_tags'], 
+        Categories_Tags = wishlist_dict['wishlist']['Categories_Tags'], 
+        Countries_en = wishlist_dict['wishlist']['Countries_en'], 
+        Ingredients_Text = wishlist_dict['wishlist']['Ingredients_Text'], 
+        Image_url = wishlist_dict['wishlist']['Image_url'], 
+        Image_Small_url = wishlist_dict['wishlist']['Image_Small_url'], 
+        Image_Ingredients_url = wishlist_dict['wishlist']['Image_Ingredients_url'],
+        Image_Ingredients_Small_url = wishlist_dict['wishlist']['Image_Ingredients_Small_url'], 
+        Image_Nutrition_url = wishlist_dict['wishlist']['Image_Nutrition_url'], 
+        Image_Nutrition_Small_url = wishlist_dict['wishlist']['Image_Nutrition_Small_url']
 
     )
 
-    return wishlist
+    return wishlist_dict
