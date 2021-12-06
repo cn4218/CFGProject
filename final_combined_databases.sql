5b2f2382d272a40afa2e7a96fdbba1660701db3a
Nasian <87439662+nasiana@users.noreply.github.com>
2021-12-06 23:38:37 +0000

    Add files via upload

--- final_combined_databases.sql
+++ final_combined_databases.sql
@@ -1,6 +1,22 @@
 CREATE DATABASE CFG_Project;
 use CFG_Project;
 
+CREATE TABLE `User_Info` (
+-- after we get an MVP, can possible implement user/password
+`User_ID` int NOT NULL UNIQUE AUTO_INCREMENT,
+`User_Name` varchar(50) NOT NULL,
+`Name_User` varchar(50) NOT NULL,
+`Email_Address` varchar(100) NOT NULL,
+CONSTRAINT PK_User PRIMARY KEY (User_Name, User_ID)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `fill_user_info`(UserName varchar(50), NameUser varchar(50), EmailAddress varchar(100))
+BEGIN
+    INSERT INTO User_Info (User_Name, Name_User, Email_Address) VALUES (UserName, NameUser, EmailAddress);
+END$$
+DELIMITER ;
+
 CREATE TABLE `Wish_List` (
 `User_ID` int,
 `User_Name` varchar(50),
@@ -94,4 +110,3 @@ BEGIN
     Image_Nutrition_Small_url);
 END$$
 DELIMITER ;
-
