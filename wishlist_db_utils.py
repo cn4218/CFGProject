c0323b7fe3a1cfca9c925d466c2dcdb141a190a4
Nasian <87439662+nasiana@users.noreply.github.com>
2021-12-06 22:45:52 +0000

    Add files via upload

--- wishlist_db_utils.py
+++ wishlist_db_utils.py
@@ -69,30 +69,48 @@ def add_wish_list(UserID, UserName, ProductID, Code_Wish, Product_name, Quantity
         cur = db_connection.cursor()
         print("Connected to DB: %s" % db_name)
 
-        query = """
-                  UPDATE  wish_list
-                  SET 
-                      `{User_ID}` = '{UserID}',
-                      `{User_Name}` = '{UserName}',
-                      `{productID}` = '{ProductID}',
-                      `{code}` = '{Code_Wish}',
-                      `{product_name}` = '{Product_name}',
-                      `{quantity}` = '{Quantity}',
-                      `{brands}` = '{Brands}',
-                      `{brands_tags}` = '{Brands_tags}',
-                      `{categories_tags}` = '{Categories_Tags}',
-                      `{categories_en}` = '{Categories_En}',
-                      `{countries}` = '{Countries}',
-                      `{countries_tags}` = '{Countries_Tags}',
-                      `{countries_en}` = '{Countries_en}',
-                      `{ingredients_text}` = '{Ingredients_Text}',
-                      `{image_url}` = '{Image_url}',
-                      `{image_small_url}` = '{Image_Small_url}',
-                      `{image_ingredients_url}` = '{Image_Ingredients_url}',
-                      `{image_ingredients_small_url}` = '{Image_Ingredients_Small_url}', 
-                      `{image_nutrition_url}` = '{Image_Nutrition_url}', 
-                      `{image_nutrition_small_url}` = '{Image_Nutrition_Small_url}'
-                  WHERE `{User_ID}` = '{UserID}' AND  `{productID}` = '{ProductID}'
+        query = """ INSERT INTO wish_list ({User_ID},
+         {User_Name},
+        {productID},
+        {code},
+        {product_name},
+        {quantity},
+         {brands}, 
+        {brands_tags},
+        {categories_tags},
+        {categories_en},
+        {countries},
+        {countries_tags},
+        {countries_en},
+        {ingredients_text},
+        {image_url},
+        {image_small_url},
+         {image_ingredients_url},
+         {image_ingredients_small_url},
+        {image_nutrition_url},
+        {image_nutrition_small_url}
+        ) 
+        VALUES ( '{UserID}',
+                       '{UserName}',
+                       '{ProductID}',
+                      '{Code_Wish}',
+                       '{Product_name}',
+                      '{Quantity}',
+                      '{Brands}',
+                      ` '{Brands_tags}',
+                     '{Categories_Tags}',
+                       '{Categories_En}',
+                       '{Countries}',
+                       '{Countries_Tags}',
+                       '{Countries_en}',
+                       '{Ingredients_Text}',
+                       '{Image_url}',
+                      '{Image_Small_url}',
+                       '{Image_Ingredients_url}',
+                      '{Image_Ingredients_Small_url}', 
+                       '{Image_Nutrition_url}', 
+                       '{Image_Nutrition_Small_url}'
+                  )
                   """.format(
             User_ID = User_ID,
             UserID = UserID,
@@ -137,10 +155,15 @@ def add_wish_list(UserID, UserName, ProductID, Code_Wish, Product_name, Quantity
             User_ID = User_ID,
             UserID = UserID,
             productID = productID,
-            ProductID = ProductID
-        )
+            ProductID = ProductID)
 
-        cur.execute(query)
+        try:
+            cur.execute(query)
+        # To insert multiple rows into a table, use the executemany() method.
+        # this except error is in the case if multiple columns are being attempted to be inserted into the database
+        except:
+            cur.executemany(query)
+
         db_connection.commit()
         cur.close()
     except Exception:
@@ -201,3 +224,75 @@ def _get_wish_list_all(UserID):
 # if __name__ == "__main__":
 #
 
+
+  # the following code may become useful at some point
+        # query = """
+        #           UPDATE  wish_list
+        #           SET
+        #               `{User_ID}` = '{UserID}',
+        #               `{User_Name}` = '{UserName}',
+        #               `{productID}` = '{ProductID}',
+        #               `{code}` = '{Code_Wish}',
+        #               `{product_name}` = '{Product_name}',
+        #               `{quantity}` = '{Quantity}',
+        #               `{brands}` = '{Brands}',
+        #               `{brands_tags}` = '{Brands_tags}',
+        #               `{categories_tags}` = '{Categories_Tags}',
+        #               `{categories_en}` = '{Categories_En}',
+        #               `{countries}` = '{Countries}',
+        #               `{countries_tags}` = '{Countries_Tags}',
+        #               `{countries_en}` = '{Countries_en}',
+        #               `{ingredients_text}` = '{Ingredients_Text}',
+        #               `{image_url}` = '{Image_url}',
+        #               `{image_small_url}` = '{Image_Small_url}',
+        #               `{image_ingredients_url}` = '{Image_Ingredients_url}',
+        #               `{image_ingredients_small_url}` = '{Image_Ingredients_Small_url}',
+        #               `{image_nutrition_url}` = '{Image_Nutrition_url}',
+        #               `{image_nutrition_small_url}` = '{Image_Nutrition_Small_url}'
+        #           WHERE `{User_ID}` = '{UserID}' AND  `{productID}` = '{ProductID}'
+        #           """.format(
+        #     User_ID = User_ID,
+        #     UserID = UserID,
+        #     User_Name = User_Name,
+        #     UserName = UserName,
+        #     productID = productID,
+        #     ProductID = ProductID,
+        #     code = code,
+        #     Code_Wish = Code_Wish,
+        #     product_name = product_name,
+        #     Product_name = Product_name,
+        #     quantity = quantity,
+        #     Quantity = Quantity,
+        #     brands = brands,
+        #     Brands = Brands,
+        #     brands_tags = brands_tags,
+        #     Brands_tags = Brands_tags,
+        #     categories_tags = categories_tags,
+        #     Categories_Tags = Categories_Tags,
+        #     categories_en = categories_en,
+        #     Categories_En = Categories_En,
+        #     countries=countries,
+        #     Countries = Countries,
+        #     countries_tags = countries_tags,
+        #     Countries_Tags = Countries_Tags,
+        #     countries_en = countries_en,
+        #     Countries_en = Countries_en,
+        #     ingredients_text = ingredients_text,
+        #     Ingredients_Text = Ingredients_Text,
+        #     image_url = image_url,
+        #     Image_url = Image_url,
+        #     image_small_url = image_small_url,
+        #     Image_Small_url = Image_Small_url,
+        #     image_ingredients_url = image_ingredients_url,
+        #     Image_Ingredients_url = Image_Ingredients_url,
+        #     image_ingredients_small_url = image_ingredients_small_url,
+        #     Image_Ingredients_Small_url = Image_Ingredients_Small_url,
+        #     image_nutrition_url = image_nutrition_url,
+        #     Image_Nutrition_url = Image_Nutrition_url,
+        #     image_nutrition_small_url = image_nutrition_small_url,
+        #     Image_Nutrition_Small_url = Image_Nutrition_Small_url,
+        #     User_ID = User_ID,
+        #     UserID = UserID,
+        #     productID = productID,
+        #     ProductID = ProductID
+        # )
