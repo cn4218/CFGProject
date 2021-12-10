fc4035b06a5e59468687c3e0d5f68653d4b802b7
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-10 15:55:03 +0000

    Add files via upload

---
+++ scripts/main_wishlist.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+
+def add_new_booking():
+
+    wishlistdict = {'username':'sarah123',
+        'UserID': 3,
+        'wishlist' : {
+                'ProductID' : 6,
+                'Code_Wish': '62263436',
+                'Product_name': 'Huile de massage larnica',
+                'Quantity': '100 ml',
+                'Brands': 'Weleda',
+                'Brands_tags': 'weleda',
+                'Categories_Tags': r'en:body,en:body-oils,fr:huile-de-massage',
+                'categories_en': r'Body,Body-oils,fr:huile-de-massage',
+                'countries': 'France',
+                'Countries_Tags': 'en:france',
+                'Countries_en': 'France',
+                'Ingredients_Text': 'helianthus annuus (sunflower) seed oil, olea europaea (olive) fruit oil, fragrance*, arnica montana flower extract, betula alba leaf extract, limonene*,  linaloo*, geraniol*, coumarin* *composé présent dans les huiles essentielles naturelles',
+                'Image_url': 'https://static.openbeautyfacts.org/images/products/000/006/226/3436/front_fr.3.400.jpg',
+                'Image_Small_url': 'https://static.openbeautyfacts.org/images/products/000/006/226/3436/front_fr.3.200.jpg',
+                'Image_Ingredients_url': '',
+                'Image_Ingredients_Small_url': '',
+                'Image_Nutrition_url': '',
+                'Image_Nutrition_Small_url': ''
+            }}
+
+    result = requests.put(
+        "http://127.0.0.1:5003/wishlist/add",
+
+        headers={"content-type": "application/json"},
+        data=json.dumps(wishlistdict),
+    )
+
+
+    return result.json()
+
+
+if __name__ == '__main__':
+    add_new_booking()
+
