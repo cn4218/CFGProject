cbe4a0156e1592af723963e6713b9b3fe6e471c7
geowooll <92748377+geowooll@users.noreply.github.com>
2021-12-16 18:04:39 +0000

    Update user_info_connector.js

--- frontend/user_info_connector.js
+++ frontend/user_info_connector.js
@@ -1,25 +1,26 @@
-    var mysql      = require('mysql');
-    // new variable (connection) - create the connection
-    var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root', // insert username for db
-    password : '', // sql password
-    database : 'db_name' // database name
-    });
+// Links to user_info.sql db in temp branch
 
-    // create the connection
-    connection.connect();
+var mysql      = require('mysql');
+// new variable (connection) - create the connection
+var connection = mysql.createConnection({
+host     : 'localhost',
+user     : 'root', // insert username for db
+password : '', // sql password
+database : 'user_info' // database name
+});
 
-    // select all the data from the table (in the database)
-    connection.query('SELECT * from tablex', function(err, rows, fields) {
-    // Print the rows
-        if (!err)
-        console.log(rows);
-    
+// create the connection
+connection.connect();
+
+// select all the data from the table (in the database)
+connection.query('SELECT * from User_Info', function(err, rows, fields) {
+// Print the rows
+    if (!err)
+    console.log(rows);
     // If there's an error, log this in the console
-        else
-        console.log('Error while performing Query.');
+    else
+    console.log('Error while performing Query.');
     });
 
-    // Close the connection
-    connection.end();
+// Close the connection
+connection.end();
