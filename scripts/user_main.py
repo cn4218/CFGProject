5d2a1feec89aff7e308cb4db4a79c9d659bb3baa
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-16 20:48:15 +0000

    Update user_main.py

--- scripts/user_main.py
+++ scripts/user_main.py
@@ -38,13 +38,11 @@ class MockFrontEnd:
         return result.json()
 
 
-    def user_login(self,user_id,user_name,name,email):
+    def user_login(self,user_name,name,email):
         user = {
-            "User_ID": user_id,
             "User_Name": user_name,
             "Name_User": name,
             "Email_Address":email
-
         }
         result = requests.post(
             "http://127.0.0.1:5004/login",
@@ -76,57 +74,86 @@ class MockFrontEnd:
                     return answer
 
         answer = 'Too many tries inputting the incorrect format'
-        print(answer)
         return answer
 
 
     def enter_details(self):
-        self.username = input('Enter your username: ')
-        self.nameuser = input('Enter your name: ')
-        self.emailaddress = input('Enter your email address: ')
-        result = self.add_new_user(self.username,self.nameuser,self.emailaddress)
+        try:
+            self.username = input('Enter your username: ')
+            self.nameuser = input('Enter your name: ')
+            self.emailaddress = input('Enter your email address: ')
+            if '@' not in self.emailaddress or '.' not in self.emailaddress:
+                raise Exception
+            result = self.add_new_user(self.username,self.nameuser,self.emailaddress)
+        except:
+            print('Your email address has NOT been given in the requested format')
+            return 'Incorrect Email Input'
         return result
 
     def verify_account_added(self):
         self.user_id = get_user_id(self.username,self.nameuser,self.emailaddress)
-        verify_account = self.user_login(self.user_id, self.username,self.nameuser,self.emailaddress)
+        verify_account = self.user_login(self.username,self.nameuser,self.emailaddress)
         return verify_account
 
     def displaying_user(self):
         print('Account has been created successfully')
         display_user_details = self.get_profile_by_id(self.user_id)  
-        print(display_user_details)
         return display_user_details
 
     def deleting_account(self):
         ans = input('Would you like to delete your account, y/n? ')
         try: 
-            if ans!= 'y' and ans!='n':
+            if ans != 'y' and ans !='n':
                 raise Exception
             elif ans == 'y':
-                dict = self.delete_user_func(self.user_id)
-                if dict == {}:
+                result = self.delete_user_func(self.user_id)
+                if result == "Account successfully deleted for user {}".format(self.user_id):
                     print('Account successfully deleted') 
-            return dict
+                    return 'Account successfully deleted'
+
+            elif ans == 'n':
+                return ans
+            return result
         except:
             print('Your answer has NOT been given in the requested format')
-
+    
 
 
 def run():
     mock = MockFrontEnd()
-    answer = mock.welcome_message()
 
+    answer = mock.welcome_message()
     if answer == 'y':
-        result = mock.enter_details()
-        verify_account = mock.verify_account_added()
-        if verify_account:
-            mock.displaying_user()
-            mock.deleting_account()
-        else:
+        try:
+            result = mock.enter_details()
+            if result == 'Incorrect Email Input':
+                issue = 'Incorrect Email Input'
+                raise Exception(issue)
+            while result != 'Incorrect Email Input':
+                verify_account = mock.verify_account_added()
+                print(verify_account)
+                if verify_account != True:
+                    issue = verify_account
+                    raise Exception(issue)
+                elif verify_account:
+                    user_details = mock.displaying_user()
+                    print(result)
+                    ans = mock.deleting_account()
+                    print(ans)
+                break
+
+        except:
             print('Issue creating user')
+            run_result =  'Issue creating user: ' + issue
+            ans = run_result
+        finally:
+            if ans == 'n':
+                run_result =  result
+            else:
+                run_result = ans
+            return run_result
+
 
 
 if __name__ =='__main__':
-    run()
-
+    output = run()
