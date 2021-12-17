5259d31bc0a92e0e781c4c733624c0171e11cbe7
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-17 13:37:52 +0000

    Update user_api.py

--- scripts/user_api.py
+++ scripts/user_api.py
@@ -19,6 +19,10 @@ def change_user_name(user_id,old_user_name, new_user_name):
     result = update_user(user_id,old_user_name,new_user_name)
     return jsonify(result)
 
+@app.route('/profile/change/email/<int:user_id>/<old_user_email>/<new_user_email>')
+def change_user_email(user_id, old_user_email, new_user_email):
+    result = update_user_email(user_id, old_user_email, new_user_email)
+    return jsonify(result)
 
 ## api endpoint that adds new user by inputing dictionary in form of
 # user_dict = { 'User_Name':'sophie123','Name_User','Sophie', 'Email_Address':'sophie@gmail.com'}
