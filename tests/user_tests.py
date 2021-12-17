85f185e3fcbb56989834ce5ecbefdee1408347e0
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-17 21:13:50 +0000

    Update user_tests.py

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -43,13 +43,14 @@ class TestUserApiDb(TestCase):
 
 
     def test_user_login(self):
-        result =  self.mock.user_login('fang123','Fang','fang@gmail.com')
+        result =  self.mock.user_login('fang123','fang@gmail.com')
         expected = True
+        result = result['verify']
         self.assertEqual(expected,result)
 
     def test_user_login_false(self):
-        result =  self.mock.user_login('wronguser','Fang','fang@gmail.com')
-        expected = False
+        result =  self.mock.user_login('wronguser','fang@gmail.com')
+        expected = {'verify': False}
         self.assertEqual(expected,result)
     
 class TestMockFrontEnd(TestCase):
@@ -76,12 +77,12 @@ class TestMockFrontEnd(TestCase):
     @patch('builtins.input',side_effect = ['sophie1998','Sophie','sophiehotmail.com'])
     def test_incorrect_email(self, mock_inputs):
         result = self.mock.enter_details()
-        self.assertEqual(result,'Incorrect Email Input')
+        self.assertEqual(result,'Error creating account: Your email address has NOT been given in the requested format')
     
     @patch('builtins.input',side_effect = ['sophie1998','Sophie','sophie@hotmailcom'])
-    def test_incorrect_email(self, mock_inputs):
+    def test_incorrect_email_2(self, mock_inputs):
         result = self.mock.enter_details()
-        self.assertEqual(result,'Incorrect Email Input')
+        self.assertEqual(result,'Error creating account: Your email address has NOT been given in the requested format')
 
     
     @patch('builtins.input', side_effect = ['sophie1998','Sophie','sophie@hotmail.com'])
@@ -91,23 +92,73 @@ class TestMockFrontEnd(TestCase):
 
 
 class TestRunFunction(TestCase):
+        
+    @patch('builtins.input', side_effect = ['y', 'sally123','sal','sally@gmailcom'])
+    def test_incorrect_email(self,mock_inputs):
+        result = run()
+        self.assertEqual(result,'Issue: Error creating account: Your email address has NOT been given in the requested format' )
+
+    @patch('builtins.input', side_effect = ['y','Ayesha11','Ayesha','ayesha@live.com','n'])
+    def test_creating_user(self,mock_inputs):
+        result = run()
+        self.assertEqual(result, {'Email_Address': 'ayesha@live.com', 'Name_User': 'Ayesha', 'User_Name': 'Ayesha11'})
+
+
     @patch('builtins.input',side_effect = ['y','annie20','Annie','annie@gmail.com','y'])
     def test_deleting_user(self,mock_inputs):
         result = run()
         self.assertEqual(result, 'Account successfully deleted for username {}'.format('annie20'))
 
-    @patch('builtin.inputs',side_effects = ['y','Ayesha11','Ayesha','ayesha@live.com','n'])
-    def test_creating_user(self,mock_inputs):
+
+    @patch('builtins.input', side_effect = ['y','Ayesha11','Ayesha','ayesha@live.com'])
+    def test_creating_user2(self,mock_inputs):
         result = run()
-        self.assertEqual(result, {'Email_Address': 'ayesha@live.com', 'Name_User': 'Ayesha', 'User_Name': 'Ayesha11'})
-    
-    @patch('builtin.inputs',side_effects = ['y','sally123','sal','sally@gmailcom'])
-    def test_incorrect_email(self,mock_inputs):
+        self.assertEqual(result, 'Issue: User details already exist, try again with a new username')
+
+
+    @patch('builtins.input',return_value = 'n')
+    def test_goodbye(self,mock_input):
         result = run()
-        self.assertEqual(result,'Issue creating user: Incorrect Email Input' )
+        self.assertEqual(result,'Goodbye!')
+
+class TestDeleteUsersCreated(TestCase):
+    def setUp(self):
+        self.mock = MockFrontEnd()
+
+    def test_delete_zita_user(self):
+        zita_id = get_user_id('zita123','zita','zita@gmail.com')
+        result = self.mock.delete_user_func(zita_id)
+        expected = "Account successfully deleted for username {}".format('zita123')
+        self.assertEqual(expected,result)
+
+    def test_delete_fang_user(self):
+        fang_id = get_user_id('fang123','Fang','fang@gmail.com')
+        result = self.mock.delete_user_func(fang_id)
+        expected = "Account successfully deleted for username {}".format('fang123')
+        self.assertEqual(expected,result)
+
+    def test_delete_sophie_user(self):
+        sophie_id = get_user_id('sophie1998','Sophie','sophie@hotmail.com')
+        result = self.mock.delete_user_func(sophie_id)
+        expected = "Account successfully deleted for username {}".format('sophie1998')
+        self.assertEqual(expected,result)
+
+
+    def test_delete_ayesha_user(self):
+        ayesha_id = get_user_id('Ayesha11','Ayesha','ayesha@live.com')
+        print(ayesha_id)
+        result = self.mock.delete_user_func(ayesha_id)
+        print(result)
+        expected = "Account successfully deleted for username {}".format('Ayesha11')
+        self.assertEqual(expected,result)
+
+    def test_delete_unknown_id(self):
+        result = self.mock.delete_user_func(9999)
+        expected = "No entry in database corresponding to given user ID: 9999"
+        self.assertEqual(expected,result)
 
     
-    
+
 
 if __name__ == "__main__":
     main()
