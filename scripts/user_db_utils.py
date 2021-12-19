cec87015abd64cd7d3af191d9656e1dba9ff80dc
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-19 15:29:45 +0000

    Update user_db_utils.py

--- scripts/user_db_utils.py
+++ scripts/user_db_utils.py
@@ -1,274 +1,254 @@
 
 import mysql.connector
 from mysql.connector import Error
-from config import USER, PASSWORD, HOST
+
+class dbConnection:
+    def __init__(self,password,db_name="CFG_Project",tb_name="user_info",user='root',host='localhost'):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.tb_name = tb_name
+        self.conn()
 
 
-# not sure if we need to turn this a class and use OOP - just leaving here for now, do this later!
-# #
+    def conn(self):
+        try:
+            db = mysql.connector.connect(
+                host = self.host,
+                user = self.user,
+                password = self.password,
+                db= self.db_name,
+            )
+            self.db = db
+            print("MySQL Database Connection is Successful!!!")
+        except Error as er:
+            print("Error: '{}' ".format(er))
 
-# Chizu: At the moment I am not sure if its the user id or the name_user I should use as a form of identification. Will ask nasian after 
-# after I am done implementing functionality on the front end
+    def _add_values(self,user_account):
+        mapped = []
+        for item in user_account:
+            mapped.append({
+                "User_ID" : item[0],
+                "User_Name" : item[1],
+                "Name_User" : item[2],
+                "Email_Address" : item[3]
+            })
+        return mapped
 
+    def verify_login(self, username,emailaddress):
+        try:
+            self.conn()
+            cur = self.db.cursor()
+            cur.execute("""
+                SELECT *
+                FROM User_Info
+                WHERE User_Name = '{}'
+                and Email_Address = '{}';""".format(username, emailaddress))
 
-class DbConnectionError(Exception):
-    pass
-
-def _create_db_connection(db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host= HOST,
-            user= USER,
-            password= PASSWORD,
-#
-            database=db_name,
-        )
-        print("MySQL Database Connection is Successful")
-    except Error as er:
-        print("Error: '{}' ".format(er)) #had to change because I was getting syntax errors 
-    return connection
-
-result = _create_db_connection("CFG_Project")
-print(result)
-    # should add the values a user enters into our db
-def _add_values(user_account):
-    mapped = []
-    for item in user_account:
-        mapped.append({
-            "User_ID" : item[0],
-            "User_Name" : item[1],
-            "Name_User" : item[2],
-            "Email_Address" : item[3]
-        })
-    return mapped
-
-## returns list containing dictionary containing information for user, given the user_id 
-## e.g. [
-#   {
-#     "Email_Address": "zita@gmail.com", 
-#     "Name_User": "zita,", 
-#     "User_ID": 2, 
-#     "User_Name": "zitazee"
-#   }
-# ]
-def _get_user(User_ID):
-    user = {}
-    try:
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-
-        query = """
-        SELECT * FROM User_Info 
-        WHERE User_ID = '{}'""".format(User_ID)
-
-        cur.execute(query)
-
-        result = (cur.fetchall())
-        user = _add_values(result)
-        cur.close()
-    except Exception as err:
-        print(err)
-        raise Error("Failed to get data from Database", err)
-    finally:
-        if db_connection:
-            db_connection.close()
-            print("Database connection is closed")
-    return user
-
-## adds user to database, returns none atm
-def add_user(User_Name, Name_User, Email_Address):
-    try:
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-
-        query = """
-            INSERT INTO user_info (User_Name, Name_User, Email_Address) VALUES ( %s, %s, %s) """
-        record = (User_Name, Name_User, Email_Address)
-        cur.execute(query, record)
-        db_connection.commit()
-        print("User has been successfully inserted to user_info table")
-
-    except mysql.connector.Error as err:
-        print("Failed to insert data into MySQL table {}".format(err))
-
-    finally:
-        if db_connection:
-            db_connection.close()
-            print("MySQL connection is closed")
-
-
-## deletes user by user_id returns string describing if deleting user has been successful or not
-def delete_user(User_ID):
-    try:
-        users = {}
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        dict = _get_user(User_ID)
-        user_name = dict[0]['User_Name']
-        print("Connected to DB: %s" % db_name)
-
-        query = """
-        DELETE FROM user_info WHERE user_id = {} """.format(User_ID)
-
-        cur.execute(query)
-        db_connection.commit()
-
-        answer = "Account successfully deleted for username {}".format(user_name)
-        cur.close()
-    except Exception as err:
-        answer = "Unsuccessful deleting account for username {}".format(user_name)
-        raise DbConnectionError("Failed to read data from Database", err)
-    finally:
-        if db_connection:
-            db_connection.close()
-    return answer
-
-
-## checks old user name and updates to a new one
-def update_user(User_ID, Old_User_Name, New_User_Name):
-    result = False
-    try:
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-
-        cur.execute("""SELECT user_name FROM user_info WHERE user_id = {}""".format(User_ID))
-        result = cur.fetchall()
-        old_user = cur.fetchone()
-        old_user_check = result[0][0]
-        if Old_User_Name == old_user_check:
-            cur.execute("""UPDATE user_info SET user_name = '{}' WHERE user_id = {}""".format(New_User_Name, User_ID))
-            db_connection.commit()
-            result = True
-        else:
-            print("The current user_name is incorrect")
-            result = False
-    except Exception as err:
-        raise DbConnectionError("Failed to read data from Database", err)
-    finally:
-        if db_connection:
-            db_connection.close()
+            result = cur.fetchall()
+            print(result)
+            rowcount = len(result)
+            if rowcount == 0:  ## user doesnt exist
+                answer = {"verify": False}
+            elif rowcount == 1:  ##user exists
+                user_id = result[0][0]
+                answer = {"verify": True, "user_id": user_id}
+            elif rowcount > 1:  ## duplicate users
+                answer = 'Duplicate users'
             cur.close()
-    return result
+            print(answer)
+            print('here i am')
+            return answer
+
+        except Exception as e:
+            raise Error("Failed to read data from DB", e)
+        finally:
+            if self.conn:
+                self.db.close()
+                print("DB connection is closed")
+
+    def add_user(self, user_name, name_user, email_address):
+        try:
+            if '@' not in email_address or '.' not in email_address:
+                issue = 'Your email address has NOT been given in the requested format'    ### raises issue if email doesnt have @ or . in thr string
+                return issue
+            answer = self.verify_login(user_name,email_address)
+            if answer['verify']== False:
+                self.conn()
+                cur = self.db.cursor()
+
+                record = (user_name, name_user, email_address)
+                cur.execute("INSERT INTO user_info (User_Name, Name_User, Email_Address) VALUES (%s, %s, %s)", record)
+                self.db.commit()
+                print("User has been successfully inserted to user_info table")
+                return True
+            elif answer['verify']==True:
+                issue = 'User details already exist, try again with a new username'
+                print(issue)
+                return issue
+        except Error as err:
+            err = str(err)
+            print("Failed to insert data into MySQL table '{}'", err)
+            return err
+        finally:
+            if self.conn:
+                self.db.close()
+                print("MySQL connection is closed")
 
 
-
-def update_user_email(User_ID, Old_User_Email, New_User_Email):
-    result = False
-    try:
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-
-        cur.execute("""SELECT email_address FROM user_info WHERE user_id = {}""".format(User_ID))
-        result = cur.fetchall()
-        old_user = cur.fetchone()
-        print(result[0])
-        old_email_check = result[0][0]
-        if Old_User_Email == old_email_check:
-            cur.execute("""UPDATE user_info SET email_address = '{}' WHERE user_id = {}""".format(New_User_Email, User_ID))
-            db_connection.commit()
-            result = True
-        else:
-            print("The current user email address is incorrect")
-            result = False
-    except Exception as err:
-        raise DbConnectionError("Failed to read data from Database", err)
-    finally:
-        if db_connection:
-            db_connection.close()
+    def _get_user(self, user_id):
+        user = {}
+        try:
+            self.conn()
+            cur = self.db.cursor()
+            cur.execute("SELECT * FROM user_info WHERE user_id = %s" % user_id)
+            result = (cur.fetchall())
+            for x in result:
+                print(x)
+            result = self._add_values(result)
             cur.close()
-    return result
-
-
-# verifies if user exists in database, mainly useful for the main.py file and testing
-def verify_login(username,email_address): #this needs to be restructured . the file needs an exception handler function
-# if someone had a username like: marc'del, would it break this code?
-    try: 
-        db_name = 'CFG_Project'
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-
-        query = """
-            SELECT *
-            FROM User_Info
-            WHERE User_Name = '{}'
-            and Email_Address = '{}'
-            """.format(username,email_address)
-
-        cur.execute(query)
-
-        result = (
-            cur.fetchall()
-        )
-        print(result)
-        rowcount = len(result)
-        print(len(result))
-        if rowcount:
-            print(result[0][0]) #this will give you the user id 
-            user_id = result[0][0]
-
-        if rowcount == 0:   ## user doesnt exist
-            answer = {"verify": False} #Chizu: I changed this 
-        elif rowcount == 1:   ##user exists
-            answer = {"verify": True, "user_id": user_id} #Chizu: I changed this as well because the front end needs the userID
-
-        cur.close()
-        print(answer)
-        return answer
-
-    except Exception as e:
-        raise DbConnectionError("Failed to read data from DB",e)
-
-    finally:
-        if db_connection:
-            db_connection.close()
-            print("DB connection is closed")
+        except Error as err:
+            print("Failed to get data from Database", err)
+        finally:
+            if self.conn:
+                self.db.close()
+                print("Database connection is closed")
+        return result
 
 
 
 
- ## finds out user id given the username,name and email
-def get_user_id(username,name,email):
-    try:
-        db_name = "CFG_Project"
-        db_connection = _create_db_connection(db_name)
-        cur = db_connection.cursor()
-        print("Connected to DB: %s" % db_name)
-        # 
-        cur.execute("""SELECT user_id FROM user_info 
-        WHERE user_name = '{}' and name_user = '{}' and email_address = '{}'""".format(username,name,email))
-        result = cur.fetchall()
-        number_users = len(result)
-        if number_users == 1:
-            userid = result[0][0]
-            answer = userid
-        elif number_users == 0:
-            print('User not found')
-            answer = False
-        else:
-            print("Issue finding unique user id")
-            answer = False
-        cur.close()
-        return answer
 
-    except Exception as err:
-        raise DbConnectionError("Failed to read data from Database", err)
-    finally:
-        if db_connection:
-            db_connection.close()
+    #  new update func
+    def update_user_name(self, user_id, old_user_name, new_user_name):
+        result = False
+        try:
+            cur = self.db.cursor()
+            cur.execute("""SELECT user_name FROM user_info WHERE user_id=%s""" % user_id)
+            user = cur.fetchone()
+            print(user)
+            if user[0] == old_user_name:
+                cur.execute("""UPDATE user_info SET user_name= '%s' WHERE user_id=%s""" %(new_user_name, user_id))
+                self.db.commit()
+                result = True
+            else:
+                print("The current username is incorrect")
+                result = False
+        except Error as err:
+            raise Error("Failed to read data from Database", err)
+        finally:
+            if self.conn:
+                self.db.close()
+        return result
+
+
+    #  email update
+    def update_user_email(self, user_id, old_email, new_email):
+        result = False
+        try:
+            cur = self.db.cursor()
+            cur.execute("""SELECT email_address FROM user_info WHERE user_id=%s""" % user_id)
+            email = cur.fetchone()
+            print(email)
+            if email[0] == old_email:
+                cur.execute("""UPDATE user_info SET email_address= '%s' WHERE user_id=%s""" % (new_email, user_id))
+                self.db.commit()
+                result = True
+            else:
+                print("The current email address is incorrect")
+                result = False
+        except Error as err:
+            raise Error("Failed to read data from Database", err)
+        finally:
+            if self.conn:
+                self.db.close()
+        return result
+
+
+    # function to change name if we want to use?
+    def update_name(self, user_id, old_name, new_name):
+        result = False
+        try:
+            cur = self.db.cursor()
+            cur.execute("""SELECT name_user FROM user_info WHERE user_id=%s""" % user_id)
+            name = cur.fetchone()
+            print(name)
+            if name[0] == old_name:
+                cur.execute("""UPDATE user_info SET name_user= '%s' WHERE user_id=%s""" % (new_name, user_id))
+                self.db.commit()
+                result = True
+            else:
+                print("The current name is incorrect")
+                result = False
+        except Error as err:
+            raise Error("Failed to read data from Database", err)
+        finally:
+            if self.conn:
+                self.db.close()
+        return result
+
+
+    # new delete - seems to work
+    def delete_user(self, user_id):
+        try:
+
+            dict = self._get_user(user_id)
+            if len(dict) == 0:
+                raise UnboundLocalError
+
+            user_name = dict[0]['User_Name']
+            self.conn()
+            cur = self.db.cursor()
+
+            cur.execute("""DELETE FROM user_info WHERE user_id = {}""" .format(user_id))
+
+            self.db.commit()
+            answer = "Account successfully deleted for username {}".format(user_name)
             cur.close()
 
-
-if __name__ == '__main__':
-    verify_login("sample_nambe","sample@gmail.com")
     
+        except UnboundLocalError as err:
+            return 'No entry in database corresponding to given user ID: {}'.format(user_id)
+        except Error as err:
+            print("Failed to read data from Database", err)
+            answer = err
+        except Exception as err:
+            answer = "Unsuccessful deleting account for username {},{}".format(user_name,err)
+        finally:
+            if self.conn:
+                self.db.close()
+                print("MySQL connection is closed")
+        return answer
+
+
+
+
+    # if the username/ name/ email don't match it returns the second elif statement and may be good to have a print to get the user's info back from the id
+    def get_user_id(self, username, name, email):
+        try:
+            self.conn()
+            cur = self.db.cursor()
+            cur.execute("""SELECT user_id FROM user_info 
+            WHERE user_name = '{}' and name_user = '{}' and email_address = '{}';""".format(username, name, email))
+            result = cur.fetchall()
+            number_users = len(result)
+            if number_users == 1:
+                userid = result[0][0]
+                answer = userid
+                print(answer, username, name, email)
+            elif number_users == 0:
+                print('There are no users with the same user info')
+                answer = False
+            else:
+                print("Issue finding unique user id")
+                answer = False
+            cur.close()
+            return answer
+        except Error as err:
+            raise Error("Failed to read data from Database", err)
+        finally:
+            if self.conn:
+                self.db.close()
+
+ #had to change because I was getting syntax errors 
