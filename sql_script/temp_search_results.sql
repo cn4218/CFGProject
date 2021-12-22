7803cfc5a3966607e78eaa7e310333b3b3d64e62
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-22 19:28:18 +0000

    Update temp_search_results.sql

--- sql_script/temp_search_results.sql
+++ sql_script/temp_search_results.sql
@@ -7,10 +7,11 @@ CREATE TABLE if not exists  `search_results` (
 `List_Product_ID` MEDIUMTEXT  NOT NULL,
 PRIMARY KEY(Search_ID));
 
--- this stored procedure called `fill_user_info` to create dummy data within our database
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `fill_search_table`(SearchID int, UserID int, ListProductID MEDIUMTEXT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `fill_search_table`(SearchID int,  ListProductID MEDIUMTEXT)
 BEGIN
-    INSERT INTO search_results (Search_ID, User_ID, List_Product_ID) VALUES (SearchID, UserID, ListProductID);
+    INSERT INTO search_results (Search_ID, List_Product_ID) VALUES (SearchID, ListProductID);
 END$$
 DELIMITER ;
+
+CALL `products`.`fill_search_table`(1, '{"product_ids": list_of_products_dummy_data}');
