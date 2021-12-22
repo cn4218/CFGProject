5e366049c0cc02dacef203374ac8d879edee05d2
sofeisong <92020043+sofeisong@users.noreply.github.com>
2021-12-22 19:29:21 +0000

    Update temp_search_results.sql

--- sql_script/temp_search_results.sql
+++ sql_script/temp_search_results.sql
@@ -3,10 +3,11 @@ use products;
 CREATE TABLE if not exists  `search_results` (
 -- after we get an MVP, can possible implement user/password
 `Search_ID` int NOT NULL UNIQUE AUTO_INCREMENT,
-`User_ID` int,
 `List_Product_ID` MEDIUMTEXT  NOT NULL,
 PRIMARY KEY(Search_ID));
 
+
+-- this stored procedure called `fill_user_info` to create dummy data within our database
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `fill_search_table`(SearchID int,  ListProductID MEDIUMTEXT)
 BEGIN
